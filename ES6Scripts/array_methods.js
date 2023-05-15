bb70fcbb364bd88c4ffa2fd52a01091000bb5b37
Alejandro Regus <Aregus@appdev.yearup.org>
2023-05-15 15:57:19 -0400

    work on previous exercises

--- ES6Scripts/array_methods.js
+++ ES6Scripts/array_methods.js
@@ -1,22 +1,35 @@
-`use script`
+`use script`;
 
 let cart = [
-    {item: "Bread", price: 3.29, quantity: 2},
-    {item: "Milk", price: 4.09, quantity: 1},
-    {item: "T-Bone Steak", price: 12.99, quantity: 2},
-    {item: "Baking Potato", price: 1.89, quantity: 6},
-    {item: "Iceberg Lettuce", price: 2.06, quantity: 1},
-    {item: "Ice Cream - Vanilla", price: 6.81, quantity: 1},
-    {item: "Apples", price: 0.66, quantity: 6}
-   ];
+  { item: "Bread", price: 3.29, quantity: 2 },
+  { item: "Milk", price: 4.09, quantity: 1 },
+  { item: "T-Bone Steak", price: 12.99, quantity: 2 },
+  { item: "Baking Potato", price: 1.89, quantity: 6 },
+  { item: "Iceberg Lettuce", price: 2.06, quantity: 1 },
+  { item: "Ice Cream - Vanilla", price: 6.81, quantity: 1 },
+  { item: "Apples", price: 0.66, quantity: 6 },
+];
 
-   function listItems (ItemsOfList) {
-    return ItemsOfList.item;
-   }
+function listItems(ItemsOfList) {
+  return ItemsOfList.item;
+}
 
-   let listNames =cart.map(listItems);
+let listNames = cart.map(listItems);
 
-   let numElements = listNames.length;
-   for (let i=0; i<numElements; i++) {
-    console.log(listNames[i]);
-   }
+let numElements = listNames.length;
+for (let i = 0; i < numElements; i++) {
+  console.log(listNames[i]);
+}
+
+//cart.forEach(listItems);
+
+//---------------------------------------------
+
+//total cost of everything code
+
+function sumReducer(sum, item) {
+  return sum + item.price;
+}
+//const pricesOfItems = cart.price
+let sum = cart.reduce(sumReducer, 0);
+console.log(`sum`, sum);

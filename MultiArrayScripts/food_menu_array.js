c4123bc5799558f59be945d5e4ba4476dbccc5b3
Alejandro Regus <Aregus@appdev.yearup.org>
2023-05-16 22:23:50 -0400

    updated work

---
+++ MultiArrayScripts/food_menu_array.js
@@ -0,0 +1,29 @@
+`use strict`
+
+let menu = [
+    [
+    {item: "Sausage and Egg Biscuit", price: 3.69},
+    {item: "Bacon and Egg Biscuit", price: 3.49},
+    {item: "Ham and Egg Biscuit", price: 3.29}
+    ],
+    [ {item: "BLT sandwich", price: 5.00},
+    {item: "Omelette & Steak", price: 12.00},
+    {item: "Hamburger", price: 10.00},
+    {item: "Tomato Soup", price: 5.00}
+ ],
+    [ {item: "Rice and Steak", price: 10.00},
+{item: "Chicken and Rice", price: 8.99},
+{item: "Asopao de pollo", price: 7.00},
+{item: "Tostones con tuna", price: 5.00},
+{item: "pollo asado", price: 9.00},
+],
+];
+
+const mealIndex = 0;
+const mealNames = [`Breakfast`, `Lunch`, `Dinner`];
+
+console.log(mealNames[mealIndex], `Menu`);
+
+//finish  on githb
+
+      
---
+++ MultiArrayScripts/multi_arrays.js
@@ -0,0 +1,20 @@
+`use strict`
+
+//Exercise 1
+
+let lockerAndAccessCode = [
+    ["Locker 1", 135],
+    ["Locker 2", 159], 
+    ["Locker 3", 642]
+   ];
+
+   console.log(lockerAndAccessCode [2] [1]) ;
+// 642
+
+console.log(lockerAndAccessCode[1]);
+//locker 2, 159
+
+//Exercise 2
+
+
+
